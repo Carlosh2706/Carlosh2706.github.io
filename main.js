08a8abf86d35b466c210628a856ea113b71fe2a5
Carlosh2706 <61483225+Carlosh2706@users.noreply.github.com>
2021-01-14 18:42:22 -0600

    Made changes to the mobile device design using media queries

--- main.js
+++ main.js
@@ -17,3 +17,14 @@ $(document).ready(function(){
 		window.location.href = $(this).attr("href");
 	});
 });
+
+const wrap = document.querySelector('#wrap');
+const burger = document.querySelector('.icon-burger');
+
+burger.addEventListener('click', ()=> {
+	if (wrap.classList.contains('nav-opened')) {
+		wrap.classList.remove('nav-opened');
+	}else {
+		wrap.classList.add('nav-opened');
+	}
+})
